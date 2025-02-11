dd228e7ec78132a493c5825e74964db581f5cf54
mansiaggarwa12 <mansiaggarwal2081@gmail.com>
2025-02-11 20:24:33 +0530

    Solution to Print a Diamond Pattern

--- Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
+++ Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
@@ -1,31 +1,30 @@
 #include <stdio.h>
-int main()
-{
-    int a,b,n,c;
-    scanf("%d",&n);
-    for (a=0;a<n;a++)
-    {
-        for (b=n-a-1;b>0;b--)
-        {
-            printf(" ");
+
+int main() {
+    int n;
+    scanf("%d", &n); // Input the number of rows for the top half of the pattern
+
+    for (int a = 0; a < n; a++) { // Loop for the upper half of the pattern
+        for (int b = 0; b < (n - a - 1); b++) {
+            printf(" "); // Printing leading spaces
         }
-        for (c=0;c<(2*a)+1;c++)
-        {
-            printf("*");
+        for (int c = 0; c < (2 * a + 1); c++) {
+            printf("*"); // Printing stars
         }
-        printf("\n");
+        printf("\n"); // Newline after each row
     }
-    for (a=0;a<n;a++)
-    {
-        for (b=0;b<=a;b++)
-        {
-            printf(" ");
+
+    for (int a = n - 2; a >= 0; a--) { // Loop for the lower half of the pattern
+        for (int b = 0; b < (n - a - 1); b++) {
+            printf(" "); // Printing leading spaces
         }
-        for (c=n-(2*a)+1;c>0;c--)
-        {
-            printf("*");
+        for (int c = 0; c < (2 * a + 1); c++) {
+            printf("*"); // Printing stars
         }
-        printf("\n");
+        if (a > 0) {
+            printf("\n"); // Newline after each row, but not after the last row
+        }
     }
+
     return 0;
 }
