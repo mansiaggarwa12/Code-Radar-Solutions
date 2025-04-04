1c5a91ee0864dc7bc0fb11b88b9b49b8359b2c86
mansiaggarwa12 <mansiaggarwal2081@gmail.com>
2025-04-04 14:37:10 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,15 @@
+int fibonacciSeries(int n)
+{
+    if (n==0)
+    {
+        return 0;
+    }
+    else if (n==1)
+    {
+        return 1;
+    }
+    else
+    {
+        return fibonacciSeries(n-1)+fibonacciSeries(n-2);
+    }
+}
